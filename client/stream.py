62620cb0afae7a1d3e7d0a00a3dbdc81274f7bcf
nn <nn12011999@gmail.com>
2020-12-10 07:59:57 +0700

    new

--- client/stream.py
+++ client/stream.py
@@ -2,10 +2,43 @@ from flask import Flask, render_template, Response, request, jsonify
 import cv2
 import time
 import threading
+#from pyrebase import Firebase
+import pyrebase
+import RPi.GPIO as GPIO
+import datetime
 
-user_input = ""
-Islocked = False
-face = []
+config = {
+    "apiKey": "AIzaSyCfTKQ-5sDNvN3QfC6S4oqeKOnbEv7AxzE",
+    "authDomain": "iot-smart-home-door-lock-7ccc9.firebaseapp.com",
+    "databaseURL": "https://iot-smart-home-door-lock-7ccc9.firebaseio.com",
+    "projectId": "iot-smart-home-door-lock-7ccc9",
+    "storageBucket": "iot-smart-home-door-lock-7ccc9.appspot.com",
+    "messagingSenderId": "513607106040",
+    "appId": "1:513607106040:web:d627644390357454e76a0d"
+}
+
+#firebase = Firebase(config)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
+storage = firebase.storage()
+
+ledPin = 23 # Broadcom pin 23 (P1 pin 16)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+GPIO.output(ledPin, GPIO.LOW)
+
+class FaceAndTime:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
+
+user_input = None
+Card_check = 0
+Open_at = 0
+Islocked = True
+face = {}
+lock = threading.Lock()
 app = Flask(__name__)
 
 camera = cv2.VideoCapture(0)  # use 0 for web camera
@@ -18,14 +51,15 @@ def gen_frames():  # generate frame by frame from camera
         # Capture frame-by-frame
         success, frame = camera.read()  # read the camera frame
         if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type:image/jpeg\r\n'
-                   b'Content-Length: ' + f"{len(frame)}".encode() + b'\r\n'
-                   b'\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            continue
+        flag, buffer = cv2.imencode('.jpg', frame)
+        if not flag:
+            continue
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type:image/jpeg\r\n'
+                b'Content-Length: ' + f"{len(frame)}".encode() + b'\r\n'
+                b'\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/video_feed')
@@ -35,49 +69,89 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     """Video streaming home page."""
+#     return render_template('index.html')
 
 
 @app.route('/face-identified', methods=['POST'])
 def face_identified():
     global face
-    face = request.get_json()
-    return jsonify(success=True)
+    with lock:
+        for f in request.get_json():
+            face[f] = time.time()
+        print(face)
+        return jsonify(success=True)
 
 
-def ReadRfid(user_input, Islocked):
+def ReadRfid():
+    global user_input,Card_check,Islocked
     while True:
-        if not Islocked:
+        if Islocked and user_input == None:
             try:
-                user_input = input()
+                temp = input()
                 # Islocked = True
+                print("vui long doi")
+                temp = db.child("Users").child(temp).get()
+                if temp != None:
+                    with lock:
+                        user_input = temp.val()
+                        Card_check = time.time()
+                        print("plz wait for face recognization")
+                #print(db.child('Users').equal_to(str(user_input)).get().val().values())
             except KeyboardInterrupt:
                 break
         time.sleep(0.1)
 
 
-def LoopCheck(user_input):
-    global face
+def LoopCheck():
+    global face ,user_input,Card_check, Open_at,Islocked,lock
+    IsChange = False
     while True:
-        print(face)
-        if user_input != "" and len(face) != 0:
-            if user_input in face:
-                print("yes")
-            else:
-                print("no")
-            user_input = ""
+
+        t = time.time()
+        with lock:
+            for f in face.copy():
+                if t-face[f] > 15:
+                    face.pop(f)
+                    IsChange = True
+        if t-Card_check>15 and Card_check!=0:
+            with lock:
+                user_input = None
+                Card_check = 0
+                print("timeout")
+        if t-Open_at > 15 and Open_at!=0:
+            with lock:
+                Islocked = True
+                Open_at = 0
+                GPIO.output(ledPin, GPIO.LOW)
+                print("door close")
+        if user_input in face and Islocked == True:
+            data = {"name": face[user_input],"rfid":user_input,"time":str(datetime.datetime.now())}
+            db.child("Access").push(data)
+            with lock:
+                user_input = None
+                Card_check = 0
+                Islocked = False
+                Open_at = t 
+                GPIO.output(ledPin, GPIO.HIGH) 
+                print("door open")   
+        if IsChange:
+            print(face)
+            IsChange = False
         time.sleep(0.1)
 
 
 if __name__ == '__main__':
-    mythread = threading.Thread(target=ReadRfid, args=(user_input, Islocked,))
-    mythread.daemon = True
-    mythread.start()
+    try:
+        mythread = threading.Thread(target=ReadRfid, args=())
+        mythread.daemon = True
+        mythread.start()
 
-    mythread1 = threading.Thread(target=LoopCheck, args=(user_input,))
-    mythread1.daemon = True
-    mythread1.start()
-    app.run(host="127.0.0.1", port="8000", threaded=True)
+        mythread1 = threading.Thread(target=LoopCheck, args=())
+        mythread1.daemon = True
+        mythread1.start()
+        app.run(host="192.168.137.189", port="8000", threaded=True)
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup() # cleanup all GPIO
--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
+from flask import Flask, render_template, Response, request, jsonify
 from time import time
 import face_recognition
 import cv2
 import numpy as np
 import time
 import requests
+import threading
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
@@ -13,9 +15,11 @@ import requests
 # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
+client_url="http://192.168.137.189:8000"
+video_url=client_url+"/video_feed"
+face_url = client_url + "/face-identified"
 # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture("http://127.0.0.1:8000/video_feed")
+video_capture = cv2.VideoCapture(video_url)
 
 # Load a sample picture and learn how to recognize it.
 obama_image = face_recognition.load_image_file("obama.jpg")
@@ -34,90 +38,130 @@ known_face_names = [
     "Barack Obama",
     "Joe Biden"
 ]
+frames = ""
+lock = threading.Lock()
+app = Flask(__name__)
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = 0
+def recognition():
+    global frames,lock
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = 0
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
-    if process_this_frame == 30:
-        # Find all the faces and face encodings in the current frame of video
-        first = time.time()
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        second = time.time()
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations)
-        face_names = []
-        face2send = []
-        third = time.time()
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(
-                known_face_encodings, face_encoding)
-            name = "Unknown"
+        # Only process every other frame of video to save time
+        if process_this_frame == 30:
+            # Find all the faces and face encodings in the current frame of video
+            first = time.time()
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            second = time.time()
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
+            face_names = []
+            face2send = []
+            third = time.time()
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding)
+                name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+                # # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(
-                known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
-            if name != "Unknown":
-                face2send.append(name)
-            face_names.append(name)
-        if len(face_names) > 0:
-            x = requests.post(
-                "http://127.0.0.1:8000/face-identified", json=face2send)
-            time.sleep(0.1)
-        process_this_frame = 0
-        print("location " + str(round(second-first, 7))
-              + "   encoding " + str(round(third-second, 7))
-              + "   recognition " + str(round(time.time()-third, 7)))
-    process_this_frame += 1
+                if name != "Unknown":
+                    face2send.append(name)
+                face_names.append(name)
+            if len(face2send) > 0:
+                x = requests.post(
+                    face_url, json=face2send)
+                time.sleep(0.1)
+            process_this_frame = 0
+            # print("location " + str(round(second-first, 7))
+            #     + "   encoding " + str(round(third-second, 7))
+            #     + "   recognition " + str(round(time.time()-third, 7)))
+        process_this_frame += 1
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35),
-                      (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6),
-                    font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                        (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+        # Display the resulting image
+        #cv2.imshow('Video', frame)
+        with lock:
+            frames = frame
+    # Release handle to the webcam
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('users_info.html')
+
+def gen_frames():  # generate frame by frame from camera
+    global frames,lock
+    while True:
+        time.sleep(0.001)
+        # Capture frame-by-frame
+        with lock:
+            ret, buffer = cv2.imencode('.jpg', frames)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type:image/jpeg\r\n'
+                b'Content-Length: ' + f"{len(frame)}".encode() + b'\r\n'
+                b'\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+if __name__ == '__main__':
+    try:
+        mythread1 = threading.Thread(target=recognition, args=())
+        mythread1.daemon = True
+        mythread1.start()
+
+        app.run(host="127.0.0.1", port="5000", threaded=True)
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup() # cleanup all GPIO
+        video_capture.release()
